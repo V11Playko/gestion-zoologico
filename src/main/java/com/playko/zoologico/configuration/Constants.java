4da421e6ff8a719d2f46cd12b30fbc6a4e855870
Heinner Vega <heinnervega20@gmail.com>
2025-06-03 12:32:44 -0500

    chore(constants): agregar mensajes constantes para gestión de zonas

--- src/main/java/com/playko/zoologico/configuration/Constants.java
+++ src/main/java/com/playko/zoologico/configuration/Constants.java
@@ -17,8 +17,17 @@ public class Constants {
     public static final String SWAGGER_LICENSE_NAME_MESSAGE = "Apache 2.0";
     public static final String SWAGGER_LICENSE_URL_MESSAGE = "http://springdoc.org";
     public static final String SWAGGER_TERMS_OF_SERVICE_MESSAGE = "http://swagger.io/terms/";
+    /**
+     * Mensajes de éxito para Zona
+     */
+    public static final String ZONA_CREATED_MESSAGE = "Zona creada exitosamente.";
+    public static final String ZONA_UPDATED_MESSAGE = "Zona actualizada exitosamente.";
+    public static final String ZONA_DELETED_MESSAGE = "Zona eliminada exitosamente.";
 
     /**
      * Mensajes de excepciones
      */
+    public static final String ZONA_NOT_FOUND_MESSAGE = "Esta zona no existe.";
+    public static final String NO_DATA_FOUND_MESSAGE = "Datos no encontrados";
+    public static final String ZONA_ALREADY_EXISTS = "Ya existe una zona con ese nombre.";
 }
