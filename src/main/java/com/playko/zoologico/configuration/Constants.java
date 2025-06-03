93b3bb2e2e8e1f7e3c60f520622293ae74739bd1
Heinner Vega <heinnervega20@gmail.com>
2025-06-02 21:16:38 -0500

    chore(config): agregar configuración de beans y constantes globales

---
+++ src/main/java/com/playko/zoologico/configuration/Constants.java
@@ -0,0 +1,24 @@
+package com.playko.zoologico.configuration;
+
+public class Constants {
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String RESPONSE_MESSAGE_KEY = "Mensaje";
+
+    /**
+     * Mensajes de Swagger
+     */
+
+    public static final String SWAGGER_TITLE_MESSAGE = "User API";
+    public static final String SWAGGER_DESCRIPTION_MESSAGE = "User microservice";
+    public static final String SWAGGER_VERSION_MESSAGE = "1.0.0";
+    public static final String SWAGGER_LICENSE_NAME_MESSAGE = "Apache 2.0";
+    public static final String SWAGGER_LICENSE_URL_MESSAGE = "http://springdoc.org";
+    public static final String SWAGGER_TERMS_OF_SERVICE_MESSAGE = "http://swagger.io/terms/";
+
+    /**
+     * Mensajes de excepciones
+     */
+}
