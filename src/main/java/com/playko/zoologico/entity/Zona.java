a7d62bdc197e6eb57020a2a5426d0601b983d8a3
Heinner Vega <heinnervega20@gmail.com>
2025-06-02 20:19:17 -0500

    feat(entity): agregar entidades base del dominio del zoológico
    Incluye clases como Zona, Animal, Comentario, Especie, Usuario y Role.

---
+++ src/main/java/com/playko/zoologico/entity/Zona.java
@@ -0,0 +1,35 @@
+package com.playko.zoologico.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "zonas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Zona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nombre;
+
+    @OneToMany(mappedBy = "zona", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Especie> especies;
+}
