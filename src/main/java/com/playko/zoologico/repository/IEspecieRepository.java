c4efcb15d92b378fb91251097fe00b361f33b439
Heinner Vega <heinnervega20@gmail.com>
2025-06-03 15:07:47 -0500

    feat(especie): actualizar entidad y repositorio de especie

--- src/main/java/com/playko/zoologico/repository/IEspecieRepository.java
+++ src/main/java/com/playko/zoologico/repository/IEspecieRepository.java
@@ -4,6 +4,8 @@ import com.playko.zoologico.entity.Especie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface IEspecieRepository extends JpaRepository<Especie, Long> {
+    boolean existsByNombreIgnoreCase(String nombre);
 }
