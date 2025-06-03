2b2a5945df936ab26790a0d0415ef6a4c58940c1
Heinner Vega <heinnervega20@gmail.com>
2025-06-03 12:34:08 -0500

    feat(dto): crear DTOs de request y response para entidad Zona

---
+++ src/main/java/com/playko/zoologico/dto/request/ZonaRequestDto.java
@@ -0,0 +1,14 @@
+package com.playko.zoologico.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ZonaRequestDto {
+
+    private Long id;
+    private String nombre;
+}
