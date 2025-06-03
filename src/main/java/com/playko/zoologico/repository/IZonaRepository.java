ed756ccbed95bcc091aa48edbbde52794ceb47a7
Heinner Vega <heinnervega20@gmail.com>
2025-06-03 12:33:03 -0500

    feat(repository): agregar método existsByNombreIgnoreCase en IZonaRepository

--- src/main/java/com/playko/zoologico/repository/IZonaRepository.java
+++ src/main/java/com/playko/zoologico/repository/IZonaRepository.java
@@ -4,6 +4,9 @@ import com.playko.zoologico.entity.Zona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IZonaRepository extends JpaRepository<Zona, Long> {
+    boolean existsByNombreIgnoreCase(String nombre);
 }
