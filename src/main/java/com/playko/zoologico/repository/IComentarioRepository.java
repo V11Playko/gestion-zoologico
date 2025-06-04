233d42f1b97c50fc745e9b2cdba9e32faec8e73e
Heinner Vega <heinnervega20@gmail.com>
2025-06-03 21:58:05 -0500

    feat(comentario-repo): agregar métodos para manejar persistencia de comentarios

--- src/main/java/com/playko/zoologico/repository/IComentarioRepository.java
+++ src/main/java/com/playko/zoologico/repository/IComentarioRepository.java
@@ -1,9 +1,14 @@
 package com.playko.zoologico.repository;
 
+import com.playko.zoologico.entity.Animal;
 import com.playko.zoologico.entity.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IComentarioRepository extends JpaRepository<Comentario, Long> {
+    List<Comentario> findByAnimalAndPadreIsNullOrderByFechaAsc(Animal animal);
+    List<Comentario> findByPadreIdOrderByFechaAsc(Long padreId);
 }
