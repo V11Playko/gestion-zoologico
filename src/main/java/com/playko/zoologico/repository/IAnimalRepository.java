0a2b62c41b31c27d78b2a288005e1228a2fe5e55
Heinner Vega <heinnervega20@gmail.com>
2025-06-03 21:58:10 -0500

    refactor(animal-repo): ajustar métodos para soportar relación con comentarios

--- src/main/java/com/playko/zoologico/repository/IAnimalRepository.java
+++ src/main/java/com/playko/zoologico/repository/IAnimalRepository.java
@@ -4,6 +4,10 @@ import com.playko.zoologico.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IAnimalRepository extends JpaRepository<Animal, Long> {
+    Optional<Animal> findByNombreIgnoreCase(String nombre);
+
 }
