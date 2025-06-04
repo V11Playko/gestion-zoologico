8740ed4bc164156d2dd4db880df625b02cba7063
Heinner Vega <heinnervega20@gmail.com>
2025-06-03 19:39:27 -0500

    feat(role-repo): agregar método para buscar rol por nombre

--- src/main/java/com/playko/zoologico/repository/IRoleRepository.java
+++ src/main/java/com/playko/zoologico/repository/IRoleRepository.java
@@ -4,7 +4,10 @@ import com.playko.zoologico.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IRoleRepository extends JpaRepository<Role, Long> {
     Role findByNombre(String nombre);
+    Optional<Role> findByNombreIgnoreCase(String nombre);
 }
