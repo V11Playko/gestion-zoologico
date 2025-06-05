e7399acfa136addaf1882dfb13407fc951aa9549
Heinner Vega <heinnervega20@gmail.com>
2025-06-04 19:44:11 -0500

    feat(especie-service): implementar lógica para obtener cantidad de animales por especie

--- src/main/java/com/playko/zoologico/service/IEspecieService.java
+++ src/main/java/com/playko/zoologico/service/IEspecieService.java
@@ -1,6 +1,7 @@
 package com.playko.zoologico.service;
 
 import com.playko.zoologico.dto.request.EspecieRequestDto;
+import com.playko.zoologico.dto.response.AnimalesPorEspecieResponseDto;
 import com.playko.zoologico.dto.response.EspecieResponseDto;
 
 import java.util.List;
@@ -11,4 +12,6 @@ public interface IEspecieService {
     void crearEspecie(EspecieRequestDto requestDto);
     void editarEspecie(Long id, EspecieRequestDto requestDto);
     void eliminarEspecie(Long id);
+
+    List<AnimalesPorEspecieResponseDto> obtenerCantidadAnimalesPorEspecie();
 }
