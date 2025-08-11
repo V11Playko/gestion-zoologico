6ba35294c0e331b2e5c0b655945eecebbb145fde
V11Playko <heinnervega20@gmail.com>
2025-08-11 12:09:58 -0500

    chore: actualizar DTO y tests para reflejar cambios en relaciones

--- src/main/java/com/playko/zoologico/dto/request/UsuarioRequestDto.java
+++ src/main/java/com/playko/zoologico/dto/request/UsuarioRequestDto.java
@@ -1,5 +1,6 @@
 package com.playko.zoologico.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UsuarioRequestDto {
+    @NotBlank
     private String nombre;
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
 }
