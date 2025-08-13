0f23e82f60e85faaf73a2f2f477abbe824c86a4d
V11Playko <heinnervega20@gmail.com>
2025-08-13 16:05:29 -0500

    feat(messaging-service): agregar manejo centralizado de excepciones

---
+++ messaging-service/src/main/java/com/playko/messaging/service/exception/MessageNotSendException.java
@@ -0,0 +1,7 @@
+package com.playko.messaging.service.exception;
+
+public class MessageNotSendException extends RuntimeException {
+    public MessageNotSendException() {
+        super();
+    }
+}
