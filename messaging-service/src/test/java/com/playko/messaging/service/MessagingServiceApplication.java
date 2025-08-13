152c21611c23124f5b9b7f18d4bab521becc09ff
V11Playko <heinnervega20@gmail.com>
2025-08-13 15:51:57 -0500

    refactor(messaging-service): renombrar clases y pasar configuración a application.yml

--- messaging-service/src/test/java/com/playko/messaging/service/MessagingServiceApplication.java
+++ messaging-service/src/test/java/com/playko/messaging/service/MessagingServiceApplication.java
@@ -1,13 +1,8 @@
 package com.playko.messaging.service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
