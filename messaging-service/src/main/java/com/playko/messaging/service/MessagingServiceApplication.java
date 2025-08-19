0fcd2af119e92a5f728e71c3b1bfe871592dbc88
V11Playko <heinnervega20@gmail.com>
2025-08-19 17:45:05 -0500

    refactor(messaging-service) ajustes en aplicación, controlador y build para integración completa

--- messaging-service/src/main/java/com/playko/messaging/service/MessagingServiceApplication.java
+++ messaging-service/src/main/java/com/playko/messaging/service/MessagingServiceApplication.java
@@ -2,10 +2,14 @@ package com.playko.messaging.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
+@EnableRetry
 public class MessagingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingServiceApplication.class, args);
